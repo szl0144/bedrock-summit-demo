9bb42b5b28f8d22089959512bda6cc32e1773c7c
Shaoyi Li <szl0144@auburn.edu>
2025-06-10 09:42:38 -0400

    add an vulnerable code script

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ from aws_cdk import (
     CfnOutput
 )
 from constructs import Construct
+import sqlite3
+from sqlite3 import OperationalError
+
+# sql-injection vulnerability example for codeguru demo
+def execute_query_noncompliant(request):
+    import sqlite3
+    name = request.GET.get("name")
+    query = "SELECT * FROM Users WHERE name = " + name + ";"
+    with sqlite3.connect("example.db") as connection:
+        cursor = connection.cursor()
+        # Noncompliant: user input is used without sanitization.
+        cursor.execute(query)
+        connection.commit()
+        connection.close()
 
 class CdklambdaStack(Stack):
 
